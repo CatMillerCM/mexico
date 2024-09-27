50801ca31d397e885523bdd3a6e0ac4ebf75fb27
CatMillerCM <cat.miller@ladbiblegroup.com>
2024-09-27 23:36:57 +0100

    add country ticker

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,46 @@
+import { countries } from './countries';
+import styles from './home.module.css';
+
+const CURRENT_COUNTRY = 'Mexico';
+
 const Page = () => {
+  const getRandomCountries = (max, quantity) => {
+    const arr = [];
+
+    while (arr.length < quantity) {
+      const index = Math.floor(Math.random() * max);
+      const country = countries[index];
+      if (!arr.includes(country) && country !== CURRENT_COUNTRY) {
+        arr.push(country);
+      }
+    }
+
+    return arr;
+  }
+
+  const countryTicker = () => {
+    const randomCountries = getRandomCountries(countries.length - 1, 25);
+    randomCountries.push(CURRENT_COUNTRY);
+
+    return randomCountries;
+  };
+
+
   return (
-    <main className="home">
-      <h2 className="">Home</h2>
-      <p>Hello, world!</p>
+    <main className={styles.home}>
+      <div className={styles.header}>
+        <h2>LOGO HERE</h2>
+        <div className={styles.location}>
+          <p>I am currently in</p>
+          <div className={styles.countryTicker}>
+            <div className={styles.countryList}>
+              {countryTicker().map((country) => (
+                <p key={country}>{country}</p>)
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };

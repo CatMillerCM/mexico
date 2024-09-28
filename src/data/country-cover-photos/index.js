2784a1575062756ac229837125c834dd421330da
CatMillerCM <cat.miller@ladbiblegroup.com>
2024-09-28 21:37:07 +0100

    add country cover photos

---
+++ src/data/country-cover-photos/index.js
@@ -0,0 +1,11 @@
+import BrazilPhoto from './Brazil.jpg';
+import MexicoPhoto from './Mexico.jpg'
+import PlaceholderPhoto from './Placeholder.jpg';
+
+const countryCoverPhotos = {
+  brazil: BrazilPhoto,
+  mexico: MexicoPhoto,
+  placeholder: PlaceholderPhoto,
+};
+
+export { countryCoverPhotos };

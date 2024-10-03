9aa81127f98cbc0b2716bc03507209d4f4c60fa5
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-03 22:13:42 +0100

    use not found and not ready in url

--- src/components/organisms/blog/index.js
+++ src/components/organisms/blog/index.js
@@ -1,24 +1,50 @@
+'use client'
+
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import NotReady from '@/app/not-ready';
+import NotFound from '@/app/not-found';
+import { countriesList } from '@/data';
 import styles from './blog.module.css';
 
-const Blog = ({ countryName }) => {
+const Blog = ({ countryName, project }) => {
+
+  if (!project) {
+    return (
+      countriesList.includes(countryName)
+        ? <NotReady/>
+        : <NotFound/>
+    )
+  }
+
   return (
     <div className={styles.blog}>
       <h2 className={styles.countryName}>{countryName}</h2>
-      <h1>Project Name</h1>
-      <p className={styles.summary}>Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary Summary </p>
-      <div className={styles.inspoPhotos}></div>
-      <p className={styles.info}>Info info info info info info info</p>
-      <div className={styles.projectPhotos}></div>
-      <p>You can have a play with the project here</p>
-      <button className={styles.hosted}>HOSTED HERE</button>
+      <h1>{project.projectName}</h1>
+      <p className={styles.intro}>{project.projectIntro}</p>
+      {/* <div className={styles.inspirationImages}>
+        {project.inspirationImages.map((image) => {
+          return <Image src={image.src} alt={image.alt}/>
+        })}
+      </div> */}
+      <p className={styles.description}>{project.description}</p>
+      {/* <div className={styles.projectImages}>
+        {project.projectImages.map((image) => {
+          return <Image src={image.src} alt={image.alt}/>
+        })}
+      </div> */}
+      <p className={styles.finalThoughts}>{project.projectFinalThoughts}</p>
+      <Link className={styles.hostedLink} href={project.hostedLink}>
+        <button>HOSTED HERE</button>
+      </Link>
       <p>As always, drop me a message if you spot any bugs, have any improvement ideas, or have any suggestions for future country projects!</p>
     </div>
   )
 };
 
 Blog.propTypes = {
-  countryName: PropTypes.string.isRequired
+  countryName: PropTypes.string.isRequired,
+  project: PropTypes.object
 };
 
 export { Blog };

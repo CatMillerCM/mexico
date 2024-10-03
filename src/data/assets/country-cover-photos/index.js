302515e2432bf13f769a39d66cd488a5a6f881f7
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-03 22:11:26 +0100

    reorder assets

---
+++ src/data/assets/country-cover-photos/index.js
@@ -0,0 +1,15 @@
+import belizePhoto from './belize.jpg';
+import brazilPhoto from './brazil.jpg';
+import guatemalaPhoto from './guatemala.jpg';
+import mexicoPhoto from './mexico.jpg'
+import unitedKingdomPhoto from './united-kingdom.jpg';
+
+const countryCoverPhotos = {
+  belize: belizePhoto,
+  brazil: brazilPhoto,
+  guatemala: guatemalaPhoto,
+  mexico: mexicoPhoto,
+  'united kingdom': unitedKingdomPhoto
+};
+
+export { countryCoverPhotos };
---
+++ src/data/assets/social-icons/index.js
@@ -0,0 +1,5 @@
+import githubIcon from './github.svg';
+import instagramIcon from './instagram.svg';
+import linkedInIcon from './linkedin.svg';
+
+export { githubIcon, instagramIcon, linkedInIcon };

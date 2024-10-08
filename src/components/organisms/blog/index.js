7a272aadf195e46f7d1351ed7ba8e91abebcffed
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-08 17:42:31 +0100

    style blog

--- src/components/organisms/blog/index.js
+++ src/components/organisms/blog/index.js
@@ -2,6 +2,7 @@
 
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import Image from 'next/image';
 import NotReady from '@/app/not-ready';
 import NotFound from '@/app/not-found';
 import { countriesList } from '@/data';
@@ -19,20 +20,44 @@ const Blog = ({ countryName, project }) => {
 
   return (
     <div className={styles.blog}>
-      <h2 className={styles.countryName}>{countryName}</h2>
-      <h1>{project.projectName}</h1>
-      <p className={styles.intro}>{project.projectIntro}</p>
-      {/* <div className={styles.inspirationImages}>
+      <div className={styles.intro}>
+        <h2 className={styles.countryName}>{countryName}</h2>
+        <h1>{project.projectName}</h1>
+        <p>{project.projectIntro}</p>
+      </div>
+      <div className={styles.inspirationImages}>
         {project.inspirationImages.map((image) => {
-          return <Image src={image.src} alt={image.alt}/>
+          return (
+            <div className={styles.image}>
+              <Image 
+                src={image.src}
+                alt={image.alt}
+                width={200}
+                height={200}
+                objectFit="cover"
+              />
+              <p className={styles.caption}>{image.caption}</p>
+            </div>
+          )
         })}
-      </div> */}
-      <p className={styles.description}>{project.description}</p>
-      {/* <div className={styles.projectImages}>
+      </div>
+      <p className={styles.description}>{project.projectDescription}</p>
+      <div className={styles.projectImages}>
         {project.projectImages.map((image) => {
-          return <Image src={image.src} alt={image.alt}/>
+          return (
+            <div className={styles.image}>
+              <Image 
+                src={image.src}
+                alt={image.alt}
+                width={200}
+                height={200}
+                objectFit="cover"
+              />
+              <p className={styles.caption}>{image.caption}</p>
+            </div>
+          )
         })}
-      </div> */}
+      </div>
       <p className={styles.finalThoughts}>{project.projectFinalThoughts}</p>
       <Link className={styles.hostedLink} href={project.hostedLink}>
         <button>HOSTED HERE</button>

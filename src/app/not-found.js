92704f6f261ca852865808a2a8d65b820c8d368d
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-08 23:54:27 +0100

    adjust not found and not ready pages

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,21 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import huhCat from '@/data/assets/cat-gifs/huh-cat.gif';
-import styles from './not-found.module.css';
+import styles from './not-found-ready.module.css';
 
 const NotFound = () => {
   return (
     <main className={styles.notFound}>
-      <p>Uh oh!</p>
-      <p>Seems like you've travelled too far and got lost!</p>
+      <h2>Uh oh!</h2>
       <Image
         src={huhCat}
         alt='Gif of a cat going "huh"'
         unoptimized
       />
+      <div>
+        <p>Seems like you've travelled too far and got lost!</p>
+        <p>Head <Link href='/home' className={styles.link}>home</Link> before visiting a new country!</p>
+      </div>
     </main>
   );
 }
--- src/app/not-ready.js
+++ src/app/not-ready.js
@@ -1,18 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import sideEyeCat from '@/data/assets/cat-gifs/side-eye-cat.gif';
-import styles from './not-ready.module.css';
+import styles from './not-found-ready.module.css';
 
 const NotReady = () => {
   return (
     <main className={styles.notReady}>
-      <p>Uh oh!</p>
-      <p>Looks like there's nothing here... yet.</p>
+      <h2>Uh oh!</h2>
       <Image
         src={sideEyeCat}
         alt='Gif of a cat side-eyeing the camera'
         unoptimized
       />
-      <p>Check back soon!</p>
+      <div>
+        <p>Looks like there's nothing here... yet.</p>
+        <p>Pack your bags and check back soon.</p>
+        <p>In the meantime, head <Link href='/home' className={styles.link}>home</Link> for a rest!</p>
+      </div>
     </main>
   );
 }

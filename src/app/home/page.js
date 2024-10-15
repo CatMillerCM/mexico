a20aefa11881597bb412774a157db8024b4a4d26
CatMillerCM <cat.miller@ladbiblegroup.com>
2024-10-15 22:53:41 +0100

    wip - plane movements

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,6 +2,7 @@ import { Nav } from '@/components/atoms/nav';
 import { Header } from '@/components/organisms/header';
 import { Carousel } from '@/components/organisms/carousel';
 import { visitedCountries } from '@/data';
+import styles from './home.module.css';
 
 const Page = () => {
   return (

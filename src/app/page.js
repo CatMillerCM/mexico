1b520af7926a1580d1fbbda161a20fed0def0854
CatMillerCM <cat.miller@ladbiblegroup.com>
2024-10-18 23:53:02 +0100

    remove planetary script and replace with css and starry night

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,18 @@
 'use client';
 
-import Script from 'next/script';
 import { Plane } from '@/components/atoms/plane';
 import { LargeGlobe } from '@/components/atoms/large-globe';
 import styles from './entry.module.css';
 
 const Entry = () => {
-  const initPlanet = () => {
-    const planet = planetaryjs.planet();
-
-    planet.loadPlugin(planetaryjs.plugins.earth({
-      topojson: { file: 'https://unpkg.com/world-atlas@1.1.4/world/110m.json' },
-      oceans: { fill: 'cadetblue' },
-      land: { fill: 'wheat' },
-      borders: { stroke: 'wheat' }
-    }));
-
-    planet.projection.scale(100).translate([125, 125]);
-    const canvas = document.getElementById('globe');
-
-    planet.onDraw(() => {
-      planet.projection.rotate([Date.now() / 30, -20])
-    });
-
-    planet.draw(canvas);
-  };
-
   return (
     <main className={styles.main}>
-      <Script src="https://d3js.org/d3.v3.min.js" />
-      <Script src="https://unpkg.com/topojson@3.0.2/dist/topojson.min.js" />
-      <Script src="/js/planetaryjs.min.js" onLoad={initPlanet} />
       <div className={styles.content}>
-        <Plane entry/>
+        <div className={styles.starryNight}></div>
+        <Plane entry />
         <div className={styles.sections}>
           <section className={styles.entry}>
-            <canvas id="globe" width="250" height="250"></canvas>
+            <div className={styles.earth}></div>
             <div className={styles.logo}>
               <h2>what's my</h2>
               <h1>#COUNTRY CODE?</h1>
--- src/components/atoms/large-globe/index.js
+++ src/components/atoms/large-globe/index.js
@@ -1,32 +1,11 @@
 'use client';
 
-import Script from 'next/script';
 import { useEffect } from 'react';
 import { useTransitionRouter } from 'next-transition-router';
 import styles from './large-globe.module.css';
 
 const LargeGlobe = () => {
   const router = useTransitionRouter();
-
-  const initPlanet = () => {
-    const planet = planetaryjs.planet();
-
-    planet.loadPlugin(planetaryjs.plugins.earth({
-      topojson: { file: 'https://unpkg.com/world-atlas@1.1.4/world/110m.json' },
-      oceans: { fill: 'cadetblue' },
-      land: { fill: 'wheat' },
-      borders: { stroke: 'wheat' }
-    }));
-
-    planet.projection.scale(300).translate([350, 500]);
-    const largeCanvas = document.getElementById('large-globe');
-
-    planet.onDraw(() => {
-      planet.projection.rotate([Date.now() / 30, -20])
-    });
-
-    planet.draw(largeCanvas);
-  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -36,10 +15,7 @@ const LargeGlobe = () => {
 
   return (
     <div>
-      <Script src="https://d3js.org/d3.v3.min.js" />
-      <Script src="https://unpkg.com/topojson@3.0.2/dist/topojson.min.js" />
-      <Script src="/js/planetaryjs.min.js" onLoad={initPlanet} />
-      <canvas className={styles.largeGlobe} id="large-globe" width="1000" height="1000"></canvas>
+      <div className={styles.largeGlobe}></div>
     </div>
   );
 }

d67d50c9a88488c4e8845ba33175ed5047e43c8a
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-24 16:12:15 +0100

    bring in logos and adjust world map colour

---
+++ src/data/assets/logos/index.js
@@ -0,0 +1,6 @@
+import condensedLogo from './condensed.svg';
+import condensedBlackLogo from './condensed-black.svg';
+import fullLogo from './full.svg';
+import fullLogoBlack from './full-black.svg';
+
+export {  condensedLogo, condensedBlackLogo, fullLogo, fullLogoBlack, };

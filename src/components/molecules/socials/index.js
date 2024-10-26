e1d49ccb1e03a0ce737fca468463cb5f716d389b
Cat Miller <cat.miller@ladbiblegroup.com>
2024-10-26 12:51:36 +0100

    icon styles

--- src/components/molecules/socials/index.js
+++ src/components/molecules/socials/index.js
@@ -6,19 +6,19 @@ import styles from './socials.module.css';
 const Socials = () => {
   return (
     <div className={styles.socials}>
-      <Link className={styles.github} href="https://github.com/CatMillerCM">
+      <Link href="https://github.com/CatMillerCM">
         <Image
           src={githubIcon}
           alt={`Github - github.com/CatMillerCM`}
         />
       </Link>
-      <Link className={styles.linkedin} href="https://www.linkedin.com/in/cat-miller/">
+      <Link href="https://www.linkedin.com/in/cat-miller/">
         <Image
           src={linkedInIcon}
           alt={`LinkedIn - linkedin.com/in/cat-miller`}
         />
       </Link>
-      <Link className={styles.instagram} href="https://www.instagram.com/catcattiecat/">
+      <Link href="https://www.instagram.com/catcattiecat/">
         <Image
           src={instagramIcon}
           alt={`Instagram - instagram.com/catcattiecat`}

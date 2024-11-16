1bd2681e57a310cf56f532f7baa0cd19d5d6eb17
Cat Miller <catmiller.cm@gmail.com>
2024-11-16 16:06:46 -0600

    reset state when making new taco

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { Layer } from '@/components/molecules/choices-layer';
-import layerChoices from '@/data/layer-choices';
+import { layerChoices } from '@/data/layer-choices';
 import styles from './page.module.css';
 
 const Page = () => {
@@ -18,11 +18,20 @@ const Page = () => {
     setDisableConfirm(true);
   };
 
+  const handleNewTaco = () => {
+    setChosen({});
+    setStep(0);
+  }
+
   return (
     <main className={styles.main}>
       {step === 0 &&
       <div className={styles.entry}>
-        <button type="button" onClick={handleConfirm}>Let's Go!</button>
+        <h2>Welcome to</h2>
+        <h1>TACO BUILDER</h1>
+        <p>Inspired by the numerous taco stalls across Mexico, this mini project pays homage to all the various flavours across different states.</p>
+        <p>Enjoy building up your favourite flavours, or try something new!</p>
+        <button type="button" onClick={handleConfirm}>Create my taco!</button>
       </div>
       }
       <div className={styles.steps}>
@@ -42,8 +51,9 @@ const Page = () => {
       </div>
       {step === 5 &&
       <div className={styles.finish}>
-        <p>COMPLETE!</p>
-        <button type="button" onClick={() => setStep(0)}>Another one!</button>
+        <h2>Taco complete!</h2>
+        <p>[IMAGE OF FINILISED TACO]!</p>
+        <button type="button" onClick={handleNewTaco}>I'm still hungry... make another one!</button>
       </div>
       }
     </main>

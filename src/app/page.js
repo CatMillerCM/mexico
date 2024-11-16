a496b5db816894f75cd9b51f2d2d38c49d901749
Cat Miller <catmiller.cm@gmail.com>
2024-11-16 11:19:26 -0600

    set up initial page app

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
-import styles from './entry.module.css';
+'use client';
+
+import { useState } from 'react';
+import { StepOne } from '@/components/molecules/step-one';
+import styles from './page.module.css';
 
 const Page = () => {
+  const [confirmedStep, setConfirmedStep] = useState(0);
+  const [stepOneChoice, setStepOneChoice] = useState(null);
+
+  const handleConfirm = () => {
+    setConfirmedStep((prev) => prev++)
+  };
+
   return (
     <main className={styles.main}>
+      <div className={styles.steps}>
+        <StepOne setStepOneChoice={setStepOneChoice}/>
+      </div>
+      <div className={styles.taco}>
+        <p>{stepOneChoice}</p>
+      </div>
+      <div className={styles.confirm}>
+        <button type="button" onClick={handleConfirm}>Confirm choice</button>
+      </div>
     </main>
   );
 }

34002c6d7fc8233f6506ce580814e70bbba7f116
Cat Miller <catmiller.cm@gmail.com>
2024-12-07 16:02:17 -0600

    use button atom in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import { Layer } from '@/components/organisms/choices-layer';
+import { Button } from '@/components/atoms/button';
 import { layerChoices } from '@/data/layer-choices';
+import taco from '@/data/taco.jpg';
 import styles from './page.module.css';
 
 const Page = () => {
@@ -23,39 +26,47 @@ const Page = () => {
     setStep(0);
   }
 
-  return (
-    <main className={styles.main}>
-      {step === 0 &&
-      <div className={styles.entry}>
+  if (step === 0) {
+    return (
+      <main className={styles.main}>
         <h2>Welcome to</h2>
         <h1>TACO BUILDER</h1>
         <p>Inspired by the numerous taco stalls across Mexico, this mini project pays homage to all the various flavours across different states.</p>
         <p>Enjoy building up your favourite flavours, or try something new!</p>
-        <button type="button" onClick={handleConfirm}>Create my taco!</button>
-      </div>
-      }
-      <div className={styles.steps}>
-        {step === 1 && <Layer layer="base" step={step} choices={layerChoices.base} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
-        {step === 2 && <Layer layer="protein" step={step} choices={layerChoices.protein} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
-        {step === 3 && <Layer layer="vegetable" step={step} choices={layerChoices.vegetable} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
-        {step === 4 && <Layer layer="salsa" step={step} choices={layerChoices.salsa} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
-      </div>
-      <div className={styles.taco}>
-        <p>{chosen.base}</p>
-        <p>{chosen.protein}</p>
-        <p>{chosen.vegetable}</p>
-        <p>{chosen.salsa}</p>
-      </div>
-      <div className={styles.confirm}>
-        <button type="button" onClick={handleConfirm} disabled={disableConfirm}>Confirm choice</button>
-      </div>
-      {step === 5 &&
-      <div className={styles.finish}>
+        <Button onClick={handleConfirm} label="Create my taco"/>
+      </main>
+    )
+  }
+
+  if (step === 5) {
+    return (
+      <main className={styles.main}>
         <h2>Taco complete!</h2>
         <p>[IMAGE OF FINILISED TACO]!</p>
-        <button type="button" onClick={handleNewTaco}>I'm still hungry... make another one!</button>
+        <p>I'm still hungry...</p>
+        <Button onClick={handleNewTaco} label="Make another one!" />
+      </main>
+    )
+  }
+
+  return (
+    <main className={styles.main}>
+      {step === 1 && <Layer layer="base" step={step} choices={layerChoices.base} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
+      {step === 2 && <Layer layer="protein" step={step} choices={layerChoices.protein} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
+      {step === 3 && <Layer layer="vegetable" step={step} choices={layerChoices.vegetable} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
+      {step === 4 && <Layer layer="salsa" step={step} choices={layerChoices.salsa} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
+      <div className={styles.taco}>
+        {/* <p>{chosen.base}</p>
+        <p>{chosen.protein}</p>
+        <p>{chosen.vegetable}</p>
+        <p>{chosen.salsa}</p> */}
+        <Image
+          src={taco}
+          alt="taco"
+          unoptimized
+        />
       </div>
-      }
+      <Button onClick={handleConfirm} label="Confirm choice" disabled={disableConfirm}/>
     </main>
   );
 }

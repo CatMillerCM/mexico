68f137aec34b28fa9d2b8cf2dc5e71eba1b86d2e
Cat Miller <catmiller.cm@gmail.com>
2024-12-11 09:58:38 -0600

    import assets

---
+++ src/assets/index.js
@@ -0,0 +1,57 @@
+import aguacate from './aguacate.png';
+import alPastor from './al-pastor.png';
+import cebolla from './cebolla.png';
+import cebollaEnEscabeche from './cebolla-en-escabeche.png';
+import camaron from './camaron.png';
+import cilantro from './cilantro.png';
+import cremaMexicana from './crema-mexicana.png';
+import habanero from './habanero.png';
+import jalapenos from './jalapenos.png';
+import lechuga from './lechuga.png';
+import mole from './mole.png';
+import nopales from './nopales.png';
+import picoDeGallo from './pico-de-gallo.png';
+import pina from './pina.png';
+import polloAsado from './pollo-asado.png';
+import pescado from './pescado.png';
+import quesoFresco from './queso-fresco.png';
+import quesoOaxaca from './queso-oaxaca.png';
+import quesoVegano from './queso-vegano.png';
+import repollo from './repollo.png';
+import salsaRoja from './salsa-roja.png';
+import salsaVerde from './salsa-verde.png';
+import setas from './setas.png';
+import tomate from './tomate.png';
+import tostada from './tostada.png';
+import tortillaDeHarina from './tortilla-de-harina.png';
+import tortillaDeMaiz from './tortilla-de-maiz.png';
+
+export {
+  aguacate,
+  alPastor,
+  cebolla,
+  cebollaEnEscabeche,
+  camaron,
+  cilantro,
+  cremaMexicana,
+  habanero,
+  jalapenos,
+  lechuga,
+  mole,
+  nopales,
+  picoDeGallo,
+  pina,
+  polloAsado,
+  pescado,
+  quesoFresco,
+  quesoOaxaca,
+  quesoVegano,
+  repollo,
+  salsaRoja,
+  salsaVerde,
+  setas,
+  tomate,
+  tostada,
+  tortillaDeHarina,
+  tortillaDeMaiz
+};

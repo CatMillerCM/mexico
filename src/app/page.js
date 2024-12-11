6dcd143a5f4beeaff59d161a2b03e77912401292
Cat Miller <catmiller.cm@gmail.com>
2024-12-11 10:01:39 -0600

    add images into page on click

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { Layer } from '@/components/organisms/choices-layer';
 import { Button } from '@/components/atoms/button';
-import { layerChoices } from '@/data/layer-choices';
-import taco from '@/data/taco.jpg';
+import { layerChoices, layerImages } from '@/data/layer-choices';
+import taco from '@/assets/taco.png';
 import styles from './page.module.css';
 
 const Page = () => {
@@ -26,11 +26,18 @@ const Page = () => {
     setStep(0);
   }
 
+  // to do - extract and optimise taco images
+
   if (step === 0) {
     return (
       <main className={styles.main}>
         <h2>Welcome to</h2>
         <h1>TACO BUILDER</h1>
+        <div className={styles.tacoImages}>
+          <Image src={taco} alt="taco" unoptimized/>
+          <Image src={taco} alt="taco" unoptimized/>
+          <Image src={taco} alt="taco" unoptimized/>
+        </div>
         <p>Inspired by the numerous taco stalls across Mexico, this mini project pays homage to all the various flavours across different states.</p>
         <p>Enjoy building up your favourite flavours, or try something new!</p>
         <Button onClick={handleConfirm} label="Create my taco"/>
@@ -38,7 +45,7 @@ const Page = () => {
     )
   }
 
-  if (step === 5) {
+  if (step === 7) {
     return (
       <main className={styles.main}>
         <h2>Taco complete!</h2>
@@ -52,19 +59,25 @@ const Page = () => {
   return (
     <main className={styles.main}>
       {step === 1 && <Layer layer="base" step={step} choices={layerChoices.base} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
-      {step === 2 && <Layer layer="protein" step={step} choices={layerChoices.protein} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
-      {step === 3 && <Layer layer="vegetable" step={step} choices={layerChoices.vegetable} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
-      {step === 4 && <Layer layer="salsa" step={step} choices={layerChoices.salsa} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
+      {step === 2 && <Layer layer="protein" step={step} choices={layerChoices.protein} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
+      {step === 3 && <Layer layer="vegetables" step={step} choices={layerChoices.vegetables} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
+      {step === 4 && <Layer layer="dairy" step={step} choices={layerChoices.dairy} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
+      {step === 5 && <Layer layer="garnishes" step={step} choices={layerChoices.garnishes} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm} multiple/>}
+      {step === 6 && <Layer layer="sauces" step={step} choices={layerChoices.sauce} chosen={chosen} setChosen={setChosen} setDisableConfirm={setDisableConfirm}/>}
       <div className={styles.taco}>
-        {/* <p>{chosen.base}</p>
-        <p>{chosen.protein}</p>
-        <p>{chosen.vegetable}</p>
-        <p>{chosen.salsa}</p> */}
-        <Image
-          src={taco}
-          alt="taco"
-          unoptimized
-        />
+        {Object.keys(chosen).map((key) => {
+          return chosen[key] && chosen[key][0] !== 'Ninguno' && chosen[key].map((choice) => {
+            console.log(chosen[key], choice, "<<<<")
+            return (
+              <Image
+                src={layerImages[choice].src}
+                alt={choice}
+                layout="fill"
+                unoptimized
+              />
+            )
+          });
+        })};
       </div>
       <Button onClick={handleConfirm} label="Confirm choice" disabled={disableConfirm}/>
     </main>

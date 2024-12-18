ba1b89a4dd8dd1abf01e994b05bc5d4be65f764c
Cat Miller <catmiller.cm@gmail.com>
2024-12-17 22:49:45 -0500

    double check spanish accents and words

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,17 +2,17 @@ import aguacate from './aguacate.png';
 import alPastor from './al-pastor.png';
 import cebolla from './cebolla.png';
 import cebollaEnEscabeche from './cebolla-en-escabeche.png';
-import camaron from './camaron.png';
+import camarón from './camaron.png';
 import cilantro from './cilantro.png';
 import cremaMexicana from './crema-mexicana.png';
 import habanero from './habanero.png';
-import jalapenos from './jalapenos.png';
+import jalapeños from './jalapenos.png';
 import lechuga from './lechuga.png';
 import lime from './lime.png';
 import mole from './mole.png';
 import nopales from './nopales.png';
 import picoDeGallo from './pico-de-gallo.png';
-import pina from './pina.png';
+import piña from './pina.png';
 import polloAsado from './pollo-asado.png';
 import pescado from './pescado.png';
 import quesoFresco from './queso-fresco.png';
@@ -21,7 +21,7 @@ import quesoVegano from './queso-vegano.png';
 import repollo from './repollo.png';
 import salsaRoja from './salsa-roja.png';
 import salsaVerde from './salsa-verde.png';
-import setas from './setas.png';
+import hongos from './hongos.png';
 import tomate from './tomate.png';
 import tostada from './tostada.png';
 import tortillaDeHarina from './tortilla-de-harina.png';
@@ -32,17 +32,17 @@ export {
   alPastor,
   cebolla,
   cebollaEnEscabeche,
-  camaron,
+  camarón,
   cilantro,
   cremaMexicana,
   habanero,
-  jalapenos,
+  jalapeños,
   lechuga,
   lime,
   mole,
   nopales,
   picoDeGallo,
-  pina,
+  piña,
   polloAsado,
   pescado,
   quesoFresco,
@@ -51,7 +51,7 @@ export {
   repollo,
   salsaRoja,
   salsaVerde,
-  setas,
+  hongos,
   tomate,
   tostada,
   tortillaDeHarina,
